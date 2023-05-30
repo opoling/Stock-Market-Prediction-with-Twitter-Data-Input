1b84d05dc21a841f82c76c9ead18b3d77c5ea711
Owen Poling <owenpoling21@gmail.com>
2023-05-30 18:46:46 -0400

    Beginning of tweet collecting

---
+++ tweetsdata.py
@@ -0,0 +1,5 @@
+# pip install the snscrape and pandas. For me, I had to do "py -3 -m pip install [package]"
+# more information on snscrape is here: https://github.com/JustAnotherArchivist/snscrape
+
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
