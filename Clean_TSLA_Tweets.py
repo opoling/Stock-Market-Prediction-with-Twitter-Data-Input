8bc5a91707c5d043d165acaf91279c2319d5b384
Owen Poling <owenpoling21@gmail.com>
2023-05-31 16:24:57 -0400

    Cleaning the tweets

--- Clean_TSLA_Tweets.py
+++ Clean_TSLA_Tweets.py
@@ -1,4 +1,4 @@
-# Importing packages
+# 1. Importing packages
 import pandas as pd
 import re
 import nltk
@@ -16,5 +16,23 @@ import matplotlib.pyplot as plt
 #from wordcloud import WordCloud
 import seaborn as sns
 
+# 2. Loading dataset
 tweets_df = pd.read_csv('TSLA_tweets.csv').drop(['Unnamed: 0'], axis=1)
 tweets_df.head()
+tweets_df.date = pd.to_datetime(tweets_df.date)
+
+# 3. Cleaning the tweets
+def clean(df):
+    df['cleaned_tweet'] = df.tweet.map(lambda x: x + ' ')
+    df.cleaned_tweet = df.cleaned_tweet.map(lambda x: re.sub(r'http.*', '', x))
+    df.cleaned_tweet = df.cleaned_tweet.map(lambda x: re.sub(r'[^a-zA-Z#]', ' ', x))
+    df.cleaned_tweet = df.cleaned_tweet.map(lambda x: x.lower())
+    stopword_list = stopwords.words('english')
+    for i in range(len(df.cleaned_tweet)):
+        tokens = word_tokenize(df.cleaned_tweet[i])
+        clean_tokens = [w for w in tokens if w not in stopword_list]
+        df.cleaned_tweet[i] = clean_tokens
+
+clean(tweets_df)
+tweets_df.head()
+                                                
