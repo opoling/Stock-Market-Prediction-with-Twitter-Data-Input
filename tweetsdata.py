011c487f8ab32f2eab1d9f7d6d639b835e857db8
Owen <108338476+opoling@users.noreply.github.com>
2023-05-31 13:29:58 -0400

    Delete tweetsdata.py

--- tweetsdata.py
+++
@@ -1,5 +0,0 @@
-# pip install the snscrape and pandas. For me, I had to do "py -3 -m pip install [package]"
-# more information on snscrape is here: https://github.com/JustAnotherArchivist/snscrape
-
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
