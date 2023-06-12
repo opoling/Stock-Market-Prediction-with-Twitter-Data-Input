018bdb725a42b65c015ce5fc20560eaa2cc13431
Owen Poling <owenpoling21@gmail.com>
2023-06-12 12:55:18 -0400

    import packages

--- 2) sentiment analysis/sentiment_analysis.py
+++ 2) sentiment analysis/sentiment_analysis.py
@@ -1,1 +1,8 @@
+# import packages
+import pandas as pd
+from textblob import TextBlob
+import matplotlib.pyplot as plt
 
+# what is %matplotlib inline: https://stackoverflow.com/questions/43027980/purpose-of-matplotlib-inline
+%matplotlib inline
+import seaborn as sns
