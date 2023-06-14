738a0ddfdf68d8c4ed4c8025ad745aa4e7a0b0db
Owen Poling <owenpoling21@gmail.com>
2023-06-14 15:50:01 -0400

    Load dataset

--- 2) sentiment analysis/sentiment_analysis.py
+++ 2) sentiment analysis/sentiment_analysis.py
@@ -6,3 +6,15 @@ import matplotlib.pyplot as plt
 # what is %matplotlib inline: https://stackoverflow.com/questions/43027980/purpose-of-matplotlib-inline
 %matplotlib inline
 import seaborn as sns
+
+
+#Load dataset
+tweets_df = pd.read_csv('cleaned_tsla_tweets.csv')
+tweets_df = tweets_df.iloc[:, [0,1,4]]
+tweets_df.head()
+
+tweets_df.info()
+
+#Change date column into datetime type
+tweets_df.date=pd.to_datetime(tweets_df.date)
+tweets_df.info()
