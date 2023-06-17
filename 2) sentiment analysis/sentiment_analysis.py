4d74716d10e22b16de3428123e879165bf04c24e
Owen Poling <owenpoling21@gmail.com>
2023-06-17 16:49:44 -0400

    bar chart

--- 2) sentiment analysis/sentiment_analysis.py
+++ 2) sentiment analysis/sentiment_analysis.py
@@ -47,3 +47,14 @@ random_subset = random_subset.rename(columns={'tweet': 'Original Tweet',
 random_subset.head(5)
 
 # 5. Frequency analysis of sentiment scores
+# plotting counts of tweets for binned sentiment scores
+bins = [-1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1]
+tweets_df['bin'] = pd.cut(tweets_df['sentiment_score'], bins)
+plt.figure(figsize=(16,9))
+ax = sns.countplot(x=tweets_df['bin'])
+ax.set_ylabel('Number of Tweets', fontsize=12)
+ax.set_xlabel('Sentiment Scores', fontsize=12)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right', fontsize=14)
+ax.set_title('Binned Sentiment Scores Tweet Counts', fontsize=20)
+ax.set_ylim(0, 120000)
+plt.show()
