b45ee8644593f6f483f565dd2a64ab2f35c15bcf
Owen Poling <owenpoling21@gmail.com>
2023-06-17 16:17:31 -0400

    Preview

--- 2) sentiment analysis/sentiment_analysis.py
+++ 2) sentiment analysis/sentiment_analysis.py
@@ -31,5 +31,19 @@ tweets_df = tweets_df.dropna()
 tweets_df['sentiment_score'] = tweets_df.apply(lambda row: TextBlob(row.cleaned_tweet).sentiment[0], axis=1)
 tweets_df.head()
 
+# 4. Previewing sentiment scores of tweets
 tweets_df.cleaned_tweet[174]
 tweets_df.sentiment_score[174]
+
+# https://pandas.pydata.org/docs/user_guide/options.html
+pd.options.display.max_colwidth = 250
+random_subset = tweets_df.sample(n=15)
+# remove columns
+random_subset = random_subset.drop(columns=['id','date'])
+random_subset = random_subset.rename(columns={'tweet': 'Original Tweet', 
+                                              'cleaned_tweet': 'Cleaned Tweet', 
+                                              'sentiment_score':'Sentiment Score'})
+
+random_subset.head(5)
+
+# 5. Frequency analysis of sentiment scores
